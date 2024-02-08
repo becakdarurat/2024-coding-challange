5d67875caeb8c8d03ea9bac760ae76332af93589
SofyanTanjung <sofyant757@gmail.com>
2024-02-08 21:47:42 +0700

    1-grow.js mengalikan 2 buah argument x dan n dalam satu function

---
+++ 1-grow.js
@@ -0,0 +1,27 @@
+
+function countBy(x, n) {
+
+    // Buat array kosong untuk menyimpan hasil
+    const result = [];
+
+    // Loop dari satu hingga n
+    for(let i = 1; i <= n; i++){
+        // kalikan i dengan x
+        const value = i * x;
+
+        // Tambahkan nilai ke array hasil
+        result.push(value);
+    }
+    // Kembalikan array hasil
+    return result;
+  }
+
+
+    // ini kita coba cara yang lebih advance
+    function countBy(x){
+        const result = x.reduce((acc, curr) => acc * curr, 1);
+        return result;
+    }
+
+    console.log(countBy([1,10]));
+    console.log(countBy([2,10]));
